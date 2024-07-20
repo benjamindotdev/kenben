46459d8aac2130b0b8d1d5d722d09873d8cbd23a
benjaminelliott <benjamin.elliott.be@gmail.com>
2024-07-20 09:35:56 +0200

    added avatars:

--- src/components/NavbarLogos.tsx
+++ src/components/NavbarLogos.tsx
@@ -1,14 +1,33 @@
 import ReactLogo from "../assets/react.svg";
 import TailwindLogo from "../assets/tailwind.svg";
 import ShadCnLogo from "../assets/shadcn.png";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+const avatars = [
+  {
+    src: ReactLogo,
+    alt: "React Logo",
+  },
+  {
+    src: TailwindLogo,
+    alt: "Tailwind Logo",
+  },
+  {
+    src: ShadCnLogo,
+    alt: "Shadcn Logo",
+  },
+];
 
 const NavbarLogos = () => {
   return (
-    <div className="flex flex-row gap-6">
-      <img src={ReactLogo} alt="React Logo" className="w-8 h-8" />
-      <img src={TailwindLogo} alt="Tailwind Logo" className="w-8 h-8" />
-      <img src={ShadCnLogo} alt="Shadcn Logo" className="w-8 h-8" />
-    </div>
+    <ul className="flex flex-row gap-4">
+      {avatars.map((avatar, index) => (
+        <Avatar key={index}>
+          <AvatarImage src={avatar.src} alt={avatar.alt} className="w-8 h-8" />
+          <AvatarFallback>{avatar.alt}</AvatarFallback>
+        </Avatar>
+      ))}
+    </ul>
   );
 };
 
