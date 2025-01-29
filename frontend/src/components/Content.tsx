51161300abdd2c2c1a7ec13e1c3cc3dd14c9eca7
benjamindotdev <hello@benjamin.dev>
2025-01-29 15:30:19 +0100

    adding login route

--- frontend/src/components/Content.tsx
+++ frontend/src/components/Content.tsx
@@ -3,12 +3,22 @@ import ContentList from "./ContentList";
 import ContentFullPage from "./ContentFullPage";
 import AddItemForm from "./AddItemForm";
 import EditItemForm from "./EditItemForm";
+import Login from "./Login";
+import { useSession } from "../context/SessionContext";
 
 const Content = () => {
+
+  const {loggedIn} = useSession();
+
+  if (!loggedIn) {
+    return <Login />;
+  }
+
   return (
     <div className="w-[65%]">
       <Routes>
         <Route path="/" element={<ContentList />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/add" element={<AddItemForm />} />
         <Route path="/:status" element={<ContentList />} />
         <Route path="/:status/:id" element={<ContentFullPage />} />
