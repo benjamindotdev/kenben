de7f08cad2c5b131ed568d623544d1f6d14ebb4a
benjamindotdev <hello@benjamin.dev>
2025-01-29 15:49:50 +0100

    typo

--- frontend/src/components/SignUp.tsx
+++ frontend/src/components/SignUp.tsx
@@ -1,9 +1,9 @@
 import SignUpForm from "@/components/SignUpForm";
 
-const SigmUp = () => {
+const SignUp = () => {
     return (
         <SignUpForm />
     );
 };
 
-    export default SigmUp;
+    export default SignUp;
