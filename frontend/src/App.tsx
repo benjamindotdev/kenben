728323a6565432e5450fe5fa82b8fe0ccb31a268
benjamindotdev <hello@benjamin.dev>
2025-01-29 15:29:49 +0100

    adding session wrapper

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,18 +3,21 @@ import { ItemsProvider } from "@/context/ItemsContext";
 import { ActiveProvider } from "@/context/ActiveContext";
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
+import { SessionProvider } from "./context/SessionContext";
 
 function App() {
   return (
     <div
       className={`flex flex-col justify-center align-middle h-[100vh] w-[100vw] mx-auto xl:w-[80vw]`}
     >
+      <SessionProvider>
       <ItemsProvider>
         <Navbar />
         <ActiveProvider>
           <Main />
         </ActiveProvider>
       </ItemsProvider>
+      </SessionProvider>
     </div>
   );
 }
