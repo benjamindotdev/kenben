e1e758a2772b5fd651d6f355d009b440594b1ac3
benjamindotdev <hello@benjamin.dev>
2025-01-29 09:11:00 +0100

    hiding unless logged in

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,15 +1,23 @@
 import { useItems } from "../context/ItemsContext";
+import { useSession } from "../context/SessionContext";
 import SidebarList from "./SidebarList";
 
 const Sidebar = () => {
   const { state } = useItems();
+  const { loggedIn } = useSession();
   const { toDos, inProgress, done, backlog } = state;
   return (
     <div className="w-[25%] flex flex-col gap-2 justify-start ">
-      <SidebarList status="Done" url="done" state={done} />
-      <SidebarList status="In Progress" url="inprogress" state={inProgress} />
-      <SidebarList status="To Do" url="todo" state={toDos} />
-      <SidebarList status="Backlog" url="backlog" state={backlog} />
+      {
+        loggedIn && (
+          <>
+            <SidebarList status="Done" url="done" state={done} />
+            <SidebarList status="In Progress" url="inprogress" state={inProgress} />
+            <SidebarList status="To Do" url="todo" state={toDos} />
+            <SidebarList status="Backlog" url="backlog" state={backlog} />
+          </>
+        )
+      }
     </div>
   );
 };
