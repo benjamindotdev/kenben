3c908476e9cf2feb37d0359d6da9d7a153f870ed
benjamindotdev <hello@benjamin.dev>
2025-02-02 09:53:48 +0100

    rebuilding page

--- frontend/src/components/Account.tsx
+++ frontend/src/components/Account.tsx
@@ -1,4 +1,5 @@
 import { useSession } from "@/context/SessionContext"
+import { Button } from "./ui/button";
 
 const Account = () => {
 
@@ -7,9 +8,23 @@ const Account = () => {
     return (
         <>
             {loggedIn ? (
-                <div>
-                    <h1>{username}</h1>
-                    <h2>{email}</h2>
+                <div className="grid grid-cols-1 grid-rows-4 gap-6 items-start justify-center">
+                    <h1 className="text-2xl">Account</h1>
+                    <div className="grid grid-cols-12 items-center">
+                        <h1 className="col-span-4">Username:</h1>
+                        <p className="col-span-6"> {username}</p>
+                        <Button className="col-span-2">Edit</Button>
+                    </div>
+                    <div className="grid grid-cols-12 items-center">
+                        <h1 className="col-span-4">Email:</h1>
+                        <p className="col-span-6"> {email}</p>
+                        <Button className="col-span-2">Edit</Button>
+                    </div>
+                    <div className="grid grid-cols-12 items-center">
+                        <h1 className="col-span-4">Password:</h1>
+                        <p className="col-span-6">*********</p>
+                        <Button className="col-span-2">Edit</Button>
+                    </div>
                 </div>
             ) : (
                 <h1>Not logged in</h1>
