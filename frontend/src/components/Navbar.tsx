3920b85da4f512db8a9765ef54f8e238e520bce1
benjamindotdev <hello@benjamin.dev>
2025-02-02 09:03:37 +0100

    hide until logged in

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -23,68 +23,73 @@ import { useSession } from "@/context/SessionContext";
 
 const Navbar = () => {
 
-  const { logOut, username } = useSession();
+  const { loggedIn, logOut, username } = useSession();
   const navigate = useNavigate();
   const handleLogOut = () => {
-    logOut();
-    navigate("/signup");
+    const check = logOut();
+    check && navigate("/signup");
   }
-
   return (
-    <nav className="w-[100%] h-[10vh] px-6 flex flex-row justify-between items-center opacity-50 hover:opacity-100 transition-all ease-in-out duration-1000">
-      <div className="flex flex-row gap-6 items-center">
-      <h1 className="navbar__title">けんべん (kenben)</h1>
-      <Sheet>
-          <SheetTrigger>About</SheetTrigger>
-          <SheetContent side={"top"} className="h-[50vw] bg-slate-900 pt-8">
-            <SheetHeader className="h-full">
-              <SheetTitle>
-                <h1 className="navbar__title">けんべん (kenben)</h1>
-              </SheetTitle>
-              <SheetDescription className="flex flex-col gap-8 h-[100%]">
-                <h2>A kanban board built by someone named Ben.</h2>
-                <div className="flex flex-row p-6 justify-start items-center gap-4">
-                  <p>Built by:</p>
-                  <HoverCard>
-                    <HoverCardTrigger>
-                      <Avatar className="animate-pulse">
-                        <AvatarImage src={benjaminGrey} alt="benjamin" />
-                        <AvatarFallback>BM</AvatarFallback>
-                      </Avatar>
-                    </HoverCardTrigger>
-                    <HoverCardContent className="w-full ml-6 mt-6">
-                      <div className="flex flex-col gap-4">
-                        <div className="flex flex-row justify-start gap-6 items-center px-2">
-                          <Avatar>
-                            <AvatarImage src={benjamin} alt="benjamin" />
-                            <AvatarFallback>BM</AvatarFallback>
-                          </Avatar>
-                          <div className="flex flex-col gap-1">
-                            <h3 className="text-primary font-bold text-xl">
-                              Benjamin
-                            </h3>
-                            <p>React Developer</p>
+    <>
+    {
+      loggedIn && (
+        <nav className="w-[100%] h-[10vh] px-6 flex flex-row justify-between items-center opacity-50 hover:opacity-100 transition-all ease-in-out duration-1000">
+        <div className="flex flex-row gap-6 items-center">
+        <h1 className="navbar__title">けんべん (kenben)</h1>
+        <Sheet>
+            <SheetTrigger>About</SheetTrigger>
+            <SheetContent side={"top"} className="h-[50vw] bg-slate-900 pt-8">
+              <SheetHeader className="h-full">
+                <SheetTitle>
+                  <h1 className="navbar__title">けんべん (kenben)</h1>
+                </SheetTitle>
+                <SheetDescription className="flex flex-col gap-8 h-[100%]">
+                  <h2>A kanban board built by someone named Ben.</h2>
+                  <div className="flex flex-row p-6 justify-start items-center gap-4">
+                    <p>Built by:</p>
+                    <HoverCard>
+                      <HoverCardTrigger>
+                        <Avatar className="animate-pulse">
+                          <AvatarImage src={benjaminGrey} alt="benjamin" />
+                          <AvatarFallback>BM</AvatarFallback>
+                        </Avatar>
+                      </HoverCardTrigger>
+                      <HoverCardContent className="w-full ml-6 mt-6">
+                        <div className="flex flex-col gap-4">
+                          <div className="flex flex-row justify-start gap-6 items-center px-2">
+                            <Avatar>
+                              <AvatarImage src={benjamin} alt="benjamin" />
+                              <AvatarFallback>BM</AvatarFallback>
+                            </Avatar>
+                            <div className="flex flex-col gap-1">
+                              <h3 className="text-primary font-bold text-xl">
+                                Benjamin
+                              </h3>
+                              <p>React Developer</p>
+                            </div>
                           </div>
+                          <SocialIcons />
                         </div>
-                        <SocialIcons />
-                      </div>
-                    </HoverCardContent>
-                  </HoverCard>
-                  <p>with:</p>
-                  <NavbarLogos />
-                </div>
-              </SheetDescription>
-            </SheetHeader>
-            <SheetFooter></SheetFooter>
-          </SheetContent>
-        </Sheet>
-      </div>
-      <div className="flex flex-row gap-6 items-center">
-        <h1>Hey, {username}</h1>
-        <Link to="/account">Account</Link>
-        <Button onClick={handleLogOut}>Log out</Button>
-      </div>
-    </nav>
+                      </HoverCardContent>
+                    </HoverCard>
+                    <p>with:</p>
+                    <NavbarLogos />
+                  </div>
+                </SheetDescription>
+              </SheetHeader>
+              <SheetFooter></SheetFooter>
+            </SheetContent>
+          </Sheet>
+        </div>
+        <div className="flex flex-row gap-6 items-center">
+          <h1>Hey, {username}</h1>
+          <Link to="/account">Account</Link>
+          <Button onClick={handleLogOut}>Log out</Button>
+        </div>
+      </nav>
+      )
+    }
+  </>
   );
 };
 
