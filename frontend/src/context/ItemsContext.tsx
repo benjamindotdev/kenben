af25c535118ba1cb1deb3c1e67aebb9e31fd1765
benjamindotdev <hello@benjamin.dev>
2025-02-16 09:08:48 +0100

    added scenario where no data is found

--- frontend/src/context/ItemsContext.tsx
+++ frontend/src/context/ItemsContext.tsx
@@ -6,8 +6,8 @@ import {
   Dispatch,
 } from "react";
 import axios from "axios";
-import data from "../data/data.json";
 import type { Item } from "../types/Item";
+import { useSession } from "./SessionContext";
 
 interface ItemsContextType {
   state: State;
@@ -29,29 +29,70 @@ type Action =
   | { type: "SET_DONE"; payload: Item[] }
   | { type: "SET_BACKLOG"; payload: Item[] };
 
-  // const data = await axios.get(`http://localhost:3001/items`,
-  //   {
-  //     headers:
-  //       {
-  //         Authorization: `Bearer ${localStorage.getItem("token")}`,
-  //         'Allow-Control-Allow-Origin': '*',
-  //         'Content-Type': 'application/json',
-  //         'Allow-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  //         'Allow-Control-Allow-Headers': 'Content-Type, Authorization',
-  //         'Access-Control-Allow-Origin': '*',
-  //       },
-  //   }
-  // ).then((res) => res.data);
+//const { username } = useSession();
+const username = "test";
+const token = localStorage.getItem("token") || "example";
+const port = import.meta.env.VITE_SERVER_PORT || 3001;
 
+console.log(username);
+console.log(token);
 
+  const data = await axios.get(`http://localhost:${port}/${username}`,
+    {
+      headers:
+        {
+          Authorization: `Bearer ${token}`,
+          'Allow-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json',
+          'Allow-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+          'Allow-Control-Allow-Headers': 'Content-Type, Authorization',
+          'Access-Control-Allow-Origin': '*',
+        },
+    }
+  ).then((res) => res.data);
+  console.log(data);
 
-const initialState: State = {
-  items: data.map((item: any) => ({ ...item })),
-  toDos: [],
-  inProgress: [],
-  done: [],
-  backlog: [],
-};
+  const initialState: State = {
+    items: data !== null ? data.map((item: any) => ({ ...item })) : [],
+    toDos: [],
+    inProgress: [],
+    done: [],
+    backlog: [],
+  };
+
+  const handleCreateItem = async (item: Item) => {
+    const data = await axios.post(`http://localhost:${port}/${username}`, { item },
+      {
+        headers:
+          {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+            'Allow-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json',
+            'Allow-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+            'Allow-Control-Allow-Headers': 'Content-Type, Authorization',
+            'Access-Control-Allow-Origin': '*',
+          },
+      }
+    ).then((res) => res.data);
+    return data;
+  }
+
+  const handleEditItem = async (item: Item) => {
+    const data = await axios.put(`http://localhost:${port}/${username}`, { item },
+      {
+        headers:
+          {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+            'Allow-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json',
+            'Allow-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+            'Allow-Control-Allow-Headers': 'Content-Type, Authorization',
+            'Access-Control-Allow-Origin': '*',
+          },
+      }
+    ).then((res) => res.data);
+    return data;
+  }
 
 const reducer = (state: State, action: Action): State => {
   switch (action.type) {
