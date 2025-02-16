83643e26c8ef2b7e8420b30f11dc105187ec7bc7
benjamindotdev <hello@benjamin.dev>
2025-02-16 13:11:15 +0100

    adjusting routes with new util

--- backend/src/routes/items/index.ts
+++ backend/src/routes/items/index.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import prisma from "../../db";
 import { verifyToken } from "../../utils/jwt";
+import getUsernameFromToken from "../../utils/getUsernameFromToken";
 
 const itemsRouter = express.Router();
 
 itemsRouter.get("/", async (req, res) => {
-    const { username } = req.body;
     const token = req.headers.authorization?.split(" ")[1];
 
     if (!token) {
@@ -13,47 +13,53 @@ itemsRouter.get("/", async (req, res) => {
         return;
     }
 
-    const decoded = verifyToken(token);
-
-    if (decoded !== username) {
-        res.status(403).json({ error: "Forbidden" });
-        return;
-    }
+    const username = getUsernameFromToken(token);
 
     const user = await prisma.user.findUnique({
         where: {
-            username: username
+            username
         },
         include: {
-            items: true
+            items: true,
+        }
+    });
+    if (user) {
+        user.password = "";
+        res.status(200).json(user);
+    } else {
+        res.status(404).json({ error: "User not found" });
+    }
+});
+
+itemsRouter.post("/", async (req, res) => {
+
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+        res.status(401).json({ error: "Unauthorized" });
+        return;
+    }
+
+    const username = getUsernameFromToken(token);
+
+    const user = await prisma.user.findUnique({
+        where: {
+            username
         }
     });
 
     if (!user) {
         res.status(404).json({ error: "User not found" });
-    } else {
-        res.status(200).json(user);
-    }
-});
-
-itemsRouter.post("/:username", async (req, res) => {
-    const { username } = req.params;
-
-    const token = req.headers.authorization?.split(" ")[1];
-
-    if (!token) {
-        res.status(401).json({ error: "Unauthorized" });
-        return;
-    }
-
-    const decoded = verifyToken(token);
-
-    if (decoded !== username) {
-        res.status(403).json({ error: "Forbidden" });
         return;
     }
 
     const { item } = req.body;
+
+    if (!item) {
+        res.status(400).json({ error: "Bad request, no item" });
+        return;
+    }
+
     const { title, description, assignee, status, priority, dueDate } = item;
 
     const newItem = await prisma.item.create({
@@ -71,23 +77,12 @@ itemsRouter.post("/:username", async (req, res) => {
             }
         }
     });
+
     res.json(newItem);
 });
 
-itemsRouter.put("/:username", async (req, res) => {
-    const { username } = req.params;
-    const { user } = req.body;
-    const updatedUser = await prisma.user.update({
-        where: {
-            username
-        },
-        data: user
-    });
-    res.json(updatedUser);
-});
+itemsRouter.put("/", async (req, res) => {
 
-itemsRouter.delete("/:username", async (req, res) => {
-    const { username } = req.params;
     const token = req.headers.authorization?.split(" ")[1];
 
     if (!token) {
@@ -95,19 +90,68 @@ itemsRouter.delete("/:username", async (req, res) => {
         return;
     }
 
-    const decoded = verifyToken(token);
+    const username = getUsernameFromToken(token);
 
-    if (decoded !== username) {
-        res.status(403).json({ error: "Forbidden" });
-        return;
-    }
-
-    const deletedUser = await prisma.user.delete({
+    const user = await prisma.user.findUnique({
         where: {
             username
         }
     });
-    res.json(deletedUser);
+
+    if (!user) {
+        res.status(404).json({ error: "User not found" });
+        return;
+    }
+
+    const { item } = req.body;
+
+    const updatedItem = await prisma.item.update({
+        where: {
+            id: item.id
+        },
+        data: {
+            title: item.title,
+            description: item.description,
+            assignee: item.assignee,
+            status: item.status,
+            priority: item.priority,
+            dueDate: item.dueDate
+        }
+    });
+
+    res.json(updatedItem);
+});
+
+itemsRouter.delete("/", async (req, res) => {
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+        res.status(401).json({ error: "Unauthorized" });
+        return;
+    }
+
+    const username = getUsernameFromToken(token);
+
+    const user = await prisma.user.findUnique({
+        where: {
+            username
+        }
+    });
+
+    if (!user) {
+        res.status(404).json({ error: "User not found" });
+        return;
+    }
+
+    const { item } = req.body;
+
+    const deletedItem = await prisma.item.delete({
+        where: {
+            id: item.id
+        }
+    });
+
+    res.json(deletedItem);
 });
 
 export default itemsRouter;
