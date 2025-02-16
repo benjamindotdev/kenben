ca7f4e59db71010aace3da6d12a1a4c1fbb9db3a
benjamindotdev <hello@benjamin.dev>
2025-02-16 12:52:52 +0100

    removing email

--- backend/src/routes/auth/index.ts
+++ backend/src/routes/auth/index.ts
@@ -6,87 +6,84 @@ import { hashPassword, comparePassword } from "../../utils/bcrypt";
 const authRouter = express.Router();
 
 authRouter.post("/signup", async (req, res) => {
+    console.log(req.body);
 
-console.log(req.body)
+    const { user } = req.body;
 
-const { user } = req.body;
+    if (!user) {
+        res.status(400).json({ error: "Bad request" });
+        return;
+    }
 
-if (!user) {
-    res.status(400).json({ error: "Bad request" });
-    return;
-}
+    if (!user.username || !user.password) {
+        res.status(400).json({ error: "Bad request" });
+        return;
+    }
 
-if (!user.email || !user.username || !user.password) {
-    res.status(400).json({ error: "Bad request" });
-    return;
-}
-
-console.log(user)
-const { email, username, password } = user;
-if (await prisma.user.findFirst({where: {email}})) {
-    res.status(400).json({ error: "Email already exists" });
-    return;
-}
-if (await prisma.user.findFirst({where: {username}})) {
-    res.status(400).json({ error: "Username already exists" });
-    return;
-}
-const hashedPassword = await hashPassword(password);
-const token = generateToken({ username });
-const newUser = await prisma.user.create({
-    data: {
-    email,
-    username,
-    password: hashedPassword,
-    token,
-    },
-});
-newUser && res.json({token, username});
+    console.log(user);
+    const { username, password } = user;
+    if (await prisma.user.findFirst({ where: { username } })) {
+        res.status(400).json({ error: "Username already exists" });
+        return;
+    }
+    try {
+        const hashedPassword = await hashPassword(password);
+        const token = generateToken({ username });
+        const newUser = await prisma.user.create({
+            data: {
+                username,
+                password: hashedPassword,
+                token
+            }
+        });
+        newUser && res.json({ token, username });
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error. User not created" });
+    }
 });
 
 authRouter.post("/login", async (req, res) => {
-if (!req.body.email) {
-    res.status(400).json({ error: "Bad request, no email" });
-    return;
-}
+    if (!req.body.username) {
+        res.status(400).json({ error: "Bad request, no username" });
+        return;
+    }
 
-if (!req.body.password) {
-    res.status(400).json({ error: "Bad request, no password" });
-    return;
-}
+    if (!req.body.password) {
+        res.status(400).json({ error: "Bad request, no password" });
+        return;
+    }
 
-const { email, password } = req.body;
-const user = await prisma.user.findFirst({
-    where:
-    {
-        email,
-    },
-});
+    const { username, password } = req.body;
+    const user = await prisma.user.findFirst({
+        where: {
+            username
+        }
+    });
 
-if (!user) {
-    res.status(404).json({ error: "User not found" });
-    return;
-}
+    if (!user) {
+        res.status(404).json({ error: "User not found" });
+        return;
+    }
 
-const passwordMatch = await comparePassword(password, user.password);
+    const passwordMatch = await comparePassword(password, user.password!);
 
-if (!passwordMatch) {
-    res.status(401).json({ error: "Unauthorized, password incorrect" });
-    return;
-}
+    if (!passwordMatch) {
+        res.status(401).json({ error: "Unauthorized, password incorrect" });
+        return;
+    }
 
-const token = generateToken({ email: user.email });
+    const token = generateToken({ username: user.username });
 
-await prisma.user.update({
-    where: {
-    email: user.email,
-    },
-    data: {
-    token,
-    },
-});
+    await prisma.user.update({
+        where: {
+            username
+        },
+        data: {
+            token
+        }
+    });
 
-res.json({ token, username: user.username, email: user.email });
+    res.json({ token, username: user.username });
 });
 
 export default authRouter;
